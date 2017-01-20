23cbabedbb645ee6790ad719aea26050c4e9a3e3
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-20 11:02:28 -0500

    Auto-committing 'project.js'

--- 029 - Distinct powers/project.js
+++ 029 - Distinct powers/project.js
@@ -24,8 +24,8 @@ function array_of_powers(a,b) {
 	power,i,j;
 	for(i=a;i<=b;i+=1){
 		for(j=a;j<=b;j+=1){
-			power = new BigNumber(i).toPower(j);
-			console.log(power);
+			power = new BigNumber(i);
+			console.log(power).toPower(j);
 			if(array.indexOf(power) === -1){
 				insert(power,array);
 			}
