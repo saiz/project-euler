8487ab422218564d241e8e5f042ad55e9b784a3a
Andres DB <Andres DB>
2017-01-19 21:42:15 -0500

    030

--- 030 - Digit fifth powers/project.js
+++ 030 - Digit fifth powers/project.js
@@ -1,5 +1,34 @@
-function main(){
+var args = process.argv.slice(2);
+function sum_of_pow_n(m, n) {
 	'use strict';
-	console.log('hello world');
+	var str = String(m).split(""),
+	i, total =0;
+	for(i=0;i<str.length;i+=1){
+		total += Math.pow(parseInt(str[i],10),n);
+	}
+	return total;
+
 }
-main();
+function digit_pow_array(n){
+	'use strict';
+	var array = [],
+	end  = Math.pow(10,n+2),
+	i;
+	for(i=2;i<end;i+=1){
+		if(sum_of_pow_n(i,n)===i){
+			array.push(i);
+		}
+	}
+	return array;
+}
+function main(n){
+	'use strict';
+	n = parseInt(n,10) || 0;
+	var array = digit_pow_array(n);
+	var sum = array.reduce(function(a,b){
+		return a+b;
+	});
+	console.log(array);
+	return sum;
+}
+console.log(main(args[0]));
