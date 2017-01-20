c0d3c48409b6814fe2752e687f0bc3cc9f7f12b4
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-20 15:40:28 -0500

    Auto-committing 'project.js'

--- 033 - Digit cancelling fractions/project.js
+++ 033 - Digit cancelling fractions/project.js
@@ -1,6 +1,18 @@
+function generate_array(){
+	var array = [];
+	for (i=1000;i<10000;i+=1){
+		array.push(i);
+	}
+	return array;
+}
+
 function main() {
 	'use strict';
-	var i ='test';
+	var array = generate_array(),
+		i;
+	for (i=0;i<array.length;i+=1){
+		console.log(i);
+	}
 	return i;
 }
 
