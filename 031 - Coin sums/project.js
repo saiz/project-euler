16a6fd814405b18e8cbbe1142e1ba0aa8f0619b6
Andres DB <Andres DB>
2017-01-20 00:34:21 -0500

    031, corrections

--- 031 - Coin sums/project.js
+++ 031 - Coin sums/project.js
@@ -1,16 +1,3 @@
-var args = process.argv.slice(2);
-function branches(array,index, total){
-	if(array[index] === 1){
-		return [total];
-	}
-	var i=0, rem = total / Math.floor(total / array[index]);
-	var arr = [];
-	while(i <=total){
-		arr.push(i);
-		i+= rem;
-	}
-	return arr;
-}
 function count( arr, index, remainder) {
 	if (remainder < 0) { //remainder less than 0, overshoot
 		return 0;
