892389d9a024e596cc6498f68bac32a8ac2862b2
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-20 14:15:35 -0500

    Auto-committing 'project.js'

--- 032 - Pandigital products/project.js
+++ 032 - Pandigital products/project.js
@@ -65,6 +65,7 @@ function main(start, end) {
 		i, a,b,c;
 	for(i=0;i<array.length;i+=1){
 		array[i] = String(array[i]);
+		console.log(array[i]);
 		temp = [1,1,array.length-1];
 		while(next_split(temp,temp.length)){
 			a = array[i].substring(0,temp[0]);
