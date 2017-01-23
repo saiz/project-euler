b48c966755a49dfff7f6c2d83ac3e00dbf848adb
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-23 12:13:15 -0500

    Auto-committing 'project.js'

--- 034 - Digit factorials/project.js
+++ 034 - Digit factorials/project.js
@@ -15,15 +15,13 @@ function add(a,b){
 }
 function main() {
 	'use strict';
-	var array = [], result = [], i= 3,j,temp = [], sum;
+	var array = [], result = [], i= 3,j,temp = [];
 	while(i < 2540161){
 		temp = String(i).split("");
 		for(j=0;j<temp.length;j+=1){
 			temp[j] = fact(array, parseInt(temp[j],10));
 		}
-		sum = temp.reduce(add, 0);
-
-		if(i === sum){
+		if(i === temp.reduce(add, 0)){
 			result.push(i);
 		}
 		i+=1;
