bf5c3c92d43308ae41c1acb25041ec941b1b8dec
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-23 14:40:47 -0500

    Auto-committing 'project.js'

--- 036 - Double-base palindromes/project.js
+++ 036 - Double-base palindromes/project.js
@@ -1,7 +1,24 @@
-function main() {
+function palindromes(str){
+	var i=0,
+		bool = true;
+	while((i<(str.length/2)) && bool){
+		if(str[i] !== str[str.length - i - 1]){
+			bool = false;
+		}
+	}
+	return bool;
+}
+
+function main(n) {
 	'use strict';
-	var i ='test';
-	return i;
+	var i;
+	n= parseInt(n,10);
+	for(i=1;i<n;i+=2){
+		if(palindromes(String(i))){
+			console.log(i);
+		}
+	}
+	return;
 }
 
 console.log(main(process.argv[2]));
