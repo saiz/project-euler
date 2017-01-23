985c724bb377eed3fc2390f4d279664fe8db4dfb
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-23 11:35:43 -0500

    Auto-committing 'project.js'

--- 034 - Digit factorials/project.js
+++ 034 - Digit factorials/project.js
@@ -1,3 +1,15 @@
+function fib(array,n) {
+	'use strict';
+	if(n < array.length){
+		return array[n];
+	}
+	if(array.length < 2){
+		array.push(1);
+	} else{
+		array.push( array[array.length - 2] + array[array.length - 1]);
+	}
+	fib(array,n);
+}
 function main() {
 	'use strict';
 	var i ='test';
