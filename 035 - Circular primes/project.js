1190805ad3d8cf78748d8325c0d85089eadb776a
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-23 13:59:26 -0500

    Auto-committing 'project.js'

--- 035 - Circular primes/project.js
+++ 035 - Circular primes/project.js
@@ -38,7 +38,6 @@ function insert(element, array) {
 function nextStr(ori,curr){
 	var copy = curr.slice(0) + curr[0];
 	curr = copy.slice(1);
-	console.log(curr)
 	if(curr === ori){
 		return false;
 	}
