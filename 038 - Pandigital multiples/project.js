7e8ee1b51865fd35417825d50624ccc2e0e4014f
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-24 10:30:32 -0500

    Auto-committing 'project.js'

--- 038 - Pandigital multiples/project.js
+++ 038 - Pandigital multiples/project.js
@@ -1,6 +1,30 @@
+function sumstring(n,m){
+	var str = '',
+	i=1;
+	while(str.length < m){
+		str +=String(n*i);
+		i+=1;
+	}
+	return parseInt(str,10);
+}
+function pandigital(n){
+	var str = String(n),
+		i = 1,
+		bool = true;
+	while(i<=str.length && bool){
+		if(!str.contains(i)){
+			bool = false;
+		}
+		i+=1;
+	}
+	return bool;
+}
 function main() {
 	'use strict';
-	var i ='test';
+	var i =1;
+	while(String(i).length<4){
+		console.log(sumstring(i,2));
+	}
 	return i;
 }
 
