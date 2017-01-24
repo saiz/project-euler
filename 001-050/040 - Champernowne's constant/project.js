49a1e0af0c7cf01040cf37d5606f990062428b82
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-24 11:58:18 -0500

    Auto-committing 'project.js'

--- 001-050/040 - Champernowne's constant/project.js
+++ 001-050/040 - Champernowne's constant/project.js
@@ -10,8 +10,10 @@ function main(n) {
 	'use strict';
 	n = parseInt(n) || 0;
 	var str ='',
-		i = 1;
-	return d(n,i,str);
+		i = 1, res;
+		res = d(n,i,str);
+		console.log(str)
+	return res;
 }
 
 console.log(main(process.argv[2]));
