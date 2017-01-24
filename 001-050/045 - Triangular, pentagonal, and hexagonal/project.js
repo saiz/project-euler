785828b5811eefa9f7a57971cc341d40a8f449e9
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-24 16:57:53 -0500

    Auto-committing 'project.js'

--- 001-050/045 - Triangular, pentagonal, and hexagonal/project.js
+++ 001-050/045 - Triangular, pentagonal, and hexagonal/project.js
@@ -1,7 +1,18 @@
+function t(n){
+	return n*(n+1)/2;
+}
+function p(n){
+	return n*(3*n-1)/2;
+}
+function h(n){
+	return n*(2n -1);
+}
+
 function main() {
 	'use strict';
 	var i ='test';
 	return i;
 }
+
 
 console.log(main(process.argv[2]));
