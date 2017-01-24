3a81e689d561e801272845c298c5e63d9bc82e7c
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-24 16:12:12 -0500

    Auto-committing 'project.js'

--- 001-050/044 - Pentagon numbers/project.js
+++ 001-050/044 - Pentagon numbers/project.js
@@ -1,6 +1,18 @@
+function p(n){
+	return n*(3*n -1)/2;
+}
+function pentagonal_array(){
+	var array = [],
+		i = 1;
+	while(array.length < 4){
+		array.push(p(i));
+		i+=1;
+	}
+	return array;
+}
 function main() {
 	'use strict';
-	var i ='test';
+	var array = pentagonal_array();s
 	return i;
 }
 
