e4a7b003da59a9e0a372457a580f3f33cdea198f
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-25 17:35:49 -0500

    Auto-committing 'project.js'

--- 001-050/050 - Consecutive prime sum/project.js
+++ 001-050/050 - Consecutive prime sum/project.js
@@ -24,7 +24,7 @@ function eratosthenes(n) {
 function endindex(n,sums){
 	var i=0;
 	if(sums.indexOf(n)> -1){
-		return sums[sums.indexOf(n);
+		return sums[sums.indexOf(n)];
 	}
 	while(sums[i] < n){
 		i+=1
