9eb12f3f947ca29ef05abddfd24424719c6493e6
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-25 11:25:28 -0500

    Auto-committing 'project.js'

--- 001-050/048 - Self powers/project.js
+++ 001-050/048 - Self powers/project.js
@@ -1,3 +1,20 @@
+function binary(power) {
+	'use strict';
+	var digits = [], i;
+    digits[0] = 1;
+    while (power > 0) {
+        for (i = digits.length-1; i >= 0; i-=1) {
+            digits[i] *= 2;
+            if (digits[i] > 9) {
+                digits[i] -= 10;
+                digits[i+1] = digits[i+1] || 0;
+                digits[i+1] += 1;
+            }
+        }
+        power-=1;
+    }
+    return digits;
+}
 function main() {
 	'use strict';
 	var i ='test';
