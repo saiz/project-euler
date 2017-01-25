73aa098e8c0f15db6957e33aa6f4a5f33f60ebbc
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-25 15:11:37 -0500

    Auto-committing 'project.js'

--- 001-050/049 - Prime permutations/project.js
+++ 001-050/049 - Prime permutations/project.js
@@ -106,7 +106,9 @@ function main() {
 			temp = other_perm(comb[i],comb)
 			prim_list(temp);
 			temp.sort();
-			console.log(temp);
+			if(temp.length>2){
+				console.log(temp);
+			}
 			i-=1;
 		}
 	return;
