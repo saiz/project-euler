86bec12afb26a82b27f754351205ac399312c7b5
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-25 11:39:11 -0500

    Auto-committing 'project.js'

--- 001-050/048 - Self powers/project.js
+++ 001-050/048 - Self powers/project.js
@@ -1,24 +1,4 @@
-function binary(power) {
-	'use strict';
-	var digits = [], i,j;
-    digits[0] = 1;
-    while (power > 0) {
-        for (i = digits.length-1; i >= 0; i-=1) {
-        	j =0;
-        	while(j <power){
-	            digits[i] *= power;
-	            if (digits[i] > 9) {
-	                digits[i] -= 10;
-	                digits[i+1] = digits[i+1] || 0;
-	                digits[i+1] += 1;
-	            }
-	            j+=1;
-	        }
-        }
-        power-=1;
-    }
-    return digits;
-}
+
 function main(n) {
 	'use strict';
 	n = parseInt(n,10);
@@ -31,16 +11,7 @@ function main(n) {
 			num = binary(i).reverse();
 			console.log(binary(i));
 			while(j<num.length){
-				if(result[j] === undefined){
-					result[j] = 0;
-				}
-				result[j] += num[j];
-				if (result[j] > 9) {
-	                result[j] -= 10;
-	                result[j+1] = result[j+1] || 0;
-	                result[j+1] += 1;
-	            }
-				j+=1;
+
 			}
 			i+=1;
 		}
