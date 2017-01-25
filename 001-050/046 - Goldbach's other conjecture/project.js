e8bd3d460fef86f9c72196b7328a576413cc5c9b
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-25 10:38:12 -0500

    Auto-committing 'project.js'

--- 001-050/046 - Goldbach's other conjecture/project.js
+++ 001-050/046 - Goldbach's other conjecture/project.js
@@ -22,13 +22,13 @@ function nextprime(n){
 }
 function goldbach(prime,n){
 	var i = 0,
-	bool = tr,
+	bool = true,
 	num = 0;
 	while(num < 1 && bool){
 		num = Math.sqrt((n - prime[i])/2);
 		//console.log(num);
 		if(num % 1 === 0){
-			bool = true;
+			bool = false;
 			console.log(num);
 		}
 		i+=1;
