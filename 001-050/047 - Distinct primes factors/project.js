21abd3e20e5a8a965c0679c24d08759bf5105548
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-25 11:04:39 -0500

    Auto-committing 'project.js'

--- 001-050/047 - Distinct primes factors/project.js
+++ 001-050/047 - Distinct primes factors/project.js
@@ -1,7 +1,39 @@
+function isPrime3(n) {
+	'use strict';
+	if (isNaN(n) || !isFinite(n) || n%1 || n<2){ return false;}
+	if (n%2===0) {return (n===2);}
+	if (n%3===0) {return (n===3);}
+	var m=Math.sqrt(n), i;
+	for (i=5;i<=m;i+=6) {
+		if (n%i===0)     {return false;}
+		if (n%(i+2)===0) {return false;}
+	}
+	return true;
+}
+function prime_factors(prime,n){
+	var array = [],
+		i =0;
+	while(prime[i] <= Math.sqrt(n)){
+		if(n%prime[i]){
+			array.push(i)
+		}
+		i+=1;
+	}
+	return array;
+}
 function main() {
 	'use strict';
-	var i ='test';
-	return i;
+	var prime = [],
+		i =2,
+		bool =true;
+	while(bool){
+		if(isPrime3(i)){
+			prime.push(i);
+		}
+
+		i+=1;
+	}
+	return;
 }
 
 console.log(main(process.argv[2]));
