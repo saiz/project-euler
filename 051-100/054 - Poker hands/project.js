7916dffe22e5f8f8683819c78e008b4b06ae98bf
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-26 15:29:51 -0500

    Auto-committing 'project.js'

--- 051-100/054 - Poker hands/project.js
+++ 051-100/054 - Poker hands/project.js
@@ -1,3 +1,25 @@
+// High Card: Highest value card.
+// One Pair: Two cards of the same value.
+// Two Pairs: Two different pairs.
+// Three of a Kind: Three cards of the same value.
+// Straight: All cards are consecutive values.
+// Flush: All cards of the same suit.
+// Full House: Three of a kind and a pair.
+// Four of a Kind: Four cards of the same value.
+// Straight Flush: All cards are consecutive values of same suit.
+// Royal Flush: Ten, Jack, Queen, King, Ace, in same suit.
+
+var fs = require('fs');
+function names_array(){
+	'use strict';
+	var arr = fs.readFileSync('./names.txt').toString().split(" "),
+	i;
+	for(i=0;i<arr.length;i+=1){
+	    arr[i] = arr[i].replace(/['"]+/g, '').toLowerCase();
+	}
+	return arr;
+
+}
 function main() {
 	'use strict';
 	var i ='test';
