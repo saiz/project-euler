c13c1f10017c86d65123c0cf6024a3825c1f7aaf
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-26 13:53:46 -0500

    Auto-committing 'project.js'

--- 051-100/053 - Combinatoric selections/project.js
+++ 051-100/053 - Combinatoric selections/project.js
@@ -10,11 +10,11 @@ function comb(n,r){
 function next(array, end){
 	var i = array[0],
 		j = array[1];
-	if(j -1 === 0){
+	if(array[1] -1 === 0){
 		array[0]+=1;
 		array[1]=1;
 	} else{
-		j-=1;
+		array[1]-=1;
 	}
 	if( array[0] > end){
 		return false;
