e7bd791aa32f20e5a7d4ea13463ad41eb6dad502
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-26 13:25:57 -0500

    Auto-committing 'project.js'

--- 051-100/052 - Permuted multiples/project.js
+++ 051-100/052 - Permuted multiples/project.js
@@ -1,3 +1,17 @@
+function is_permutation(first,second){
+	if(first.length !== second.length){
+		return false;
+	}
+	var bool = true,
+		i=0;
+	while(i<first.length && bool){
+		if(second.indexOf(first[i]) < 0){
+			bool = false;
+		}
+	}
+	return bool;
+}
+
 function main() {
 	'use strict';
 	var i ='test';
