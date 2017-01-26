711c6b2408f6319912fa57f2c23b283c7c4a91df
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-26 11:55:38 -0500

    Auto-committing 'project.js'

--- 051-100/051 - Prime digit replacements/project.js
+++ 051-100/051 - Prime digit replacements/project.js
@@ -2,6 +2,18 @@ function zeroPad(num, places) {
   var zero = places - num.toString().length + 1;
   return Array(+(zero > 0 && zero)).join("0") + num;
 }
+function isPrime3(n) {
+	'use strict';
+	if (isNaN(n) || !isFinite(n) || n%1 || n<2){ return false;}
+	if (n%2===0) {return (n===2);}
+	if (n%3===0) {return (n===3);}
+	var m=Math.sqrt(n), i;
+	for (i=5;i<=m;i+=6) {
+		if (n%i===0)     {return false;}
+		if (n%(i+2)===0) {return false;}
+	}
+	return true;
+}
 function main() {
 	'use strict';
 	var i= 0, j, k,l,
@@ -10,12 +22,14 @@ function main() {
 		zeros,
 		ones,
 		temp,
-		n =1000;
-	while(i <n){
+		array = [],
+		bool = true;
+	while(bool){
 		number = (i).toString(2);
 		zeros = (number.split("0").length - 1);
 		ones = (number.split("1").length - 1);
 		number = number.replace(/1/g, 'a').replace(/0/g, 'b');
+		array = [];
 		for(j=0;j<10;j+=1){
 			for(k=0;k<Math.pow(10,zeros);k+=1){
 				number_temp = number.replace(/a/g, j);
@@ -25,12 +39,20 @@ function main() {
 					number_temp = number_temp.replace('b',temp[l]);
 					l+=1;
 				}
-				console.log(number_temp," | ",number);
+				if(isPrime3(parseInt(number_temp,10))){
+					array.push(number_temp);
+				}
 			}
+		}
+		if(array.length === 8){
+			bool = false;
+			console.log(array);
+			console.log(number);
 		}
 		i+=1;
 	}
-	return;
+
+	return ;
 }
 
 console.log(main(process.argv[2]));
