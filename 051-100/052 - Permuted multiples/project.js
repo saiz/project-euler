51a47c9da454273a79c119713c952500b7b4f1b3
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-26 13:30:46 -0500

    Auto-committing 'project.js'

--- 051-100/052 - Permuted multiples/project.js
+++ 051-100/052 - Permuted multiples/project.js
@@ -25,7 +25,7 @@ function muliples(number,n){
 function is_array_perm(num,array){
 	var i =0,
 		bool=true;
-while(i<first.length && bool){
+	while(i<first.length && bool){
 		if(second.indexOf(first[i]) < 0){
 			bool = false;
 		}
