43562cdc7ef903145401f03b0906ab7dc090445e
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-26 11:34:10 -0500

    Auto-committing 'project.js'

--- 051-100/051 - Prime digit replacements/project.js
+++ 051-100/051 - Prime digit replacements/project.js
@@ -9,10 +9,10 @@ function main() {
 		number = (i).toString(2);
 		zeros = (number.split("0").length - 1);
 		ones = (number.split("1").length - 1);
-		if(i > 0)
-		for(j=0;j<10;j+=1){
-			console.log(number.replace(/1/g, j), " || ", zeros);
-		}
+		if(i > 0){
+				for(j=0;j<10;j+=1){
+					console.log(number.replace(/1/g, j), " || ", zeros);
+				}}
 		i+=1;
 	}
 	return;
