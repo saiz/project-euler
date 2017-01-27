dad0e9ef6f4d9bee70caa39cd1abbd7a08d114a5
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-27 10:48:56 -0500

    Auto-committing 'project.js'

--- 051-100/054 - Poker hands/project.js
+++ 051-100/054 - Poker hands/project.js
@@ -135,7 +135,7 @@ function main() {
 		personA = hand(array[i][0]).sort(card_sort);
 		personB = hand(array[i][1]).sort(card_sort);
 
-        console.log(twopair(personA), " | ",twopair(personB));
+        console.log(pair(personA), " | ",pair(personB));
 		i+=1;
 	}
 	return;
