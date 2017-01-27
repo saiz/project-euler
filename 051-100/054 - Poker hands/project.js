a828324234c55ee19c3de26ae513f7d6a3917af5
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-27 13:37:06 -0500

    Auto-committing 'project.js'

--- 051-100/054 - Poker hands/project.js
+++ 051-100/054 - Poker hands/project.js
@@ -133,7 +133,7 @@ function remove_pair (hand, value){
 }
 function pair_tie(personA,personB){
 	var a =remove_pair(personA,pair_value(personA)),
-		b= remove_pair(personA,pair_value(personA));
+		b= remove_pair(personB,pair_value(personB));
 	while
 }
 function flush(hand) {
