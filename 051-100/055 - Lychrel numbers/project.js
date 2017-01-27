dc3abce7773474a1b882db0ad2f08d6c5ef7b21c
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-27 14:49:11 -0500

    Auto-committing 'project.js'

--- 051-100/055 - Lychrel numbers/project.js
+++ 051-100/055 - Lychrel numbers/project.js
@@ -42,7 +42,7 @@ function main() {
 	while(next(_i,Math.pow(10,4))){
 		i = _i[0];
 		if(!palindromes(test(i,0,max))){
-			console.log(test(i,0,max), i)
+			//console.log(test(i,0,max), i)
 			count +=1;
 		}
 	}
