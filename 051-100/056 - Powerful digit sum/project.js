16123422c833bb9398639c80c181fc9c013a9b92
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-27 15:33:47 -0500

    Auto-committing 'project.js'

--- 051-100/056 - Powerful digit sum/project.js
+++ 051-100/056 - Powerful digit sum/project.js
@@ -4,7 +4,7 @@ function add(x, y) {
 }
 function main() {
 	'use strict';
-	var a,b,i,j,k,max =100, array, max =
+	var a,b,i,j,k,max =100, array, max =0, maxval
 	for(i=1;i<=max;i+=1){
 		for(j=1;j<=max;j+=1){
 			a = new BigNumber(i);
