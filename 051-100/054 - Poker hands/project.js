ac5a372578116e060065884d099998084c4fc765
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-27 12:22:20 -0500

    Auto-committing 'project.js'

--- 051-100/054 - Poker hands/project.js
+++ 051-100/054 - Poker hands/project.js
@@ -87,7 +87,7 @@ function uniq(hand) {
 	while(str.length > 0){
 		count +=1;
 		char = new RegExp(str[0],"g");
-		str.replace(char,"")
+		str = str.replace(char,"")
 		console.log(str,char)
 	}
 	return count;
