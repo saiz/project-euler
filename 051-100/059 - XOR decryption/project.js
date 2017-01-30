0fe2826e0ee059f3320d2a88e831bd28889a91b0
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-30 15:19:40 -0500

    Auto-committing 'project.js'

--- 051-100/059 - XOR decryption/project.js
+++ 051-100/059 - XOR decryption/project.js
@@ -18,7 +18,7 @@ function main(n) {
 		c = n.charCodeAt(2),
 		d = [a,b,c],
 		i=0;
-	console.log(String.fromCharCode('a'),String.fromCharCode('z'));
+	console.log('a'.charCodeAt(0),'z'.charCodeAt(0));
 	while(i<arr.length){
 
 		arr[i] = String.fromCharCode(d[i%3]^arr[i]);
