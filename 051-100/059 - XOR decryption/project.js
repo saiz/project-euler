50cdd70558b59ce450f29896e37f19572433e5d5
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-30 16:13:07 -0500

    Auto-committing 'project.js'

--- 051-100/059 - XOR decryption/project.js
+++ 051-100/059 - XOR decryption/project.js
@@ -41,7 +41,15 @@ function main(n) {
 			temp =  d[i%3] ^  parseInt(arr[i],10);
 			str += String.fromCharCode(temp);
 			i+=1;
+			var string = "sample1";
+	var re = new RegExp("^([a-z0-9]{5,})$");
+	if (re.test(string)) {
+	    console.log("Valid");
+	} else {
+	    console.log("Invalid");
+	}
 			if(temp )
+				/^[a-z\d\-_\s]+$/i
 		}
 		if(count > max){
 			max = count;
