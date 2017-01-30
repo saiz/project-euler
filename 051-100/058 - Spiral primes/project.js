5133669ff572874e88dbde47dc43eb95deb8194c
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-30 13:29:55 -0500

    Auto-committing 'project.js'

--- 051-100/058 - Spiral primes/project.js
+++ 051-100/058 - Spiral primes/project.js
@@ -20,9 +20,33 @@ function percent_prim(array){
     }
     return cnt/array.length;
 }
+function eratosthenes(n) {
+    // Eratosthenes algorithm to find all primes under n
+    var array = [], upperLimit = Math.sqrt(n), output = [];
+    // Make an array from 2 to (n - 1)
+    for (var i = 0; i < n; i++) {
+        array.push(true);
+    }
+    // Remove multiples of primes starting from 2, 3, 5,...
+    for (var i = 2; i <= upperLimit; i++) {
+        if (array[i]) {
+            for (var j = i * i; j < n; j += i) {
+                array[j] = false;
+            }
+        }
+    }
+    // All array[i] set to true are primes
+    for (var i = 2; i < n; i++) {
+        if(array[i]) {
+            output.push(i);
+        }
+    }
+    return output;
+}
 function main() {
     'use strict';
     var array = [1],
+        prime = []
         i=1,
         j=0,
         cnt=0,
