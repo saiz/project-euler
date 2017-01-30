448ec5fa69afc08ce95d9a9682756c71773d3411
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-30 13:37:14 -0500

    Auto-committing 'project.js'

--- 051-100/058 - Spiral primes/project.js
+++ 051-100/058 - Spiral primes/project.js
@@ -46,7 +46,7 @@ function eratosthenes(n) {
 function main() {
     'use strict';
     var array = [1],
-        prime = eratosthenes(Math.pow(2, 45)),
+        prime = eratosthenes(Math.pow(2, 40)),
         i=1,
         j=0,
         cnt=0,
