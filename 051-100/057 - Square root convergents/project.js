ee73e17e95211ec4fab536c95d3a13aa03cc503e
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-30 11:39:22 -0500

    Auto-committing 'project.js'

--- 051-100/057 - Square root convergents/project.js
+++ 051-100/057 - Square root convergents/project.js
@@ -8,14 +8,14 @@ function sqrt_two(n){
 function fract(a,b,m,n){
 	console.log(a,b,m,n);
 	var i;
-	if(m < n){
+	if(m <= n){
 		i = a+b;
 		return fract(b+i,i,m+1,n);
 	}
 	return [a,b];
 }
 function fraction(n){
-	return fract(3,2,0,n);
+	return fract(3,2,1,n);
 }
 function main(n) {
 	'use strict';
