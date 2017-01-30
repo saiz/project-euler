3daf4c411bc19ba538a252998b2953eb26d81329
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-30 13:20:42 -0500

    Auto-committing 'project.js'

--- 051-100/058 - Spiral primes/project.js
+++ 051-100/058 - Spiral primes/project.js
@@ -1,16 +1,39 @@
+function isPrime3(n) {
+    'use strict';
+    if (isNaN(n) || !isFinite(n) || n%1 || n<2){ return false;}
+    if (n%2===0) {return (n===2);}
+    if (n%3===0) {return (n===3);}
+    var m=Math.sqrt(n), i;
+    for (i=5;i<=m;i+=6) {
+        if (n%i===0)     {return false;}
+        if (n%(i+2)===0) {return false;}
+    }
+    return true;
+}
+function percent_prim(array){
+    var cnt = 0,
+        i;
+    for(i=0;i<array.length;i+=1){
+        if(isPrime3(array[i])){
+            cnt+=1;
+        }
+        return i/array.length;
+    }
+}
 function main() {
     'use strict';
-    var array = [],
+    var array = [1],
         i=1,
         j=0,
         cnt=0,
         bool = true;
     while(bool){
         if(cnt %4 === 0){
+            console.log(percent_prim(array))
             j+=1;
         }
         i+=(2*j);
-        console.log(i,j,cnt);
+        array.push(i);
         cnt+=1;
     }
     return i;
