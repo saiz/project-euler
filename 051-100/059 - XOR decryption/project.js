b52322c733e335ec4a28366eb5e5e1a30ecee7ff
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-30 16:15:24 -0500

    Auto-committing 'project.js'

--- 051-100/059 - XOR decryption/project.js
+++ 051-100/059 - XOR decryption/project.js
@@ -32,7 +32,7 @@ function main(n) {
 		i=0,
 		temp,
 		arr,str, count, max = 0,
-		re = new RegExp("/^[a-z\d\-_\s]+$/i");
+		re = new RegExp("^[a-z\d\-_\s]+$", "gi");
 	while(next(d,a,z)){
 		arr = cipher_array();
 		str = '';
