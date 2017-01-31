c5bc1225c7022eb0f282fe02c88cf3a670fd3eb4
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-01-31 16:20:03 -0500

    Auto-committing 'project.js'

--- 051-100/061 - Cyclical figurate numbers/project.js
+++ 051-100/061 - Cyclical figurate numbers/project.js
@@ -64,16 +64,16 @@ function main(){
 				i+=1;
 			}
 			if(i === list.length){
-				if(c!==d){
-					index[i] +=1;
-					i-=1;
-				}
 				if(index[index.length -1] === list[list.length -1].length){
 					index[0]+=1;
 					i=0;
 					for(k=1;k<index.length;k+=1){
 						index[k] = 0;
 					}
+				}
+				if(c!==d){
+					index[i] +=1;
+					i-=1;
 				}
 			}
 		}
