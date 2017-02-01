9aee5fab1febc8adf9edc25b6cccb0fa02a3fc58
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-01 13:38:39 -0500

    Auto-committing 'project.js'

--- 051-100/062 - Cubic permutations/project.js
+++ 051-100/062 - Cubic permutations/project.js
@@ -47,7 +47,7 @@ function main() {
 			while(nextPerm(index)){
 				console.log(index,i);
 				for(k=0;k<index.length;k+=1){
-					console.log(String(i)[index[k]]);
+					str[k] =String(i)[index[k]];
 				}
 			}
 			str.join('');
