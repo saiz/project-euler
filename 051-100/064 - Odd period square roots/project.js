24b6075cd8e1a7352b0d560cdfad751375b28300
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-01 16:26:33 -0500

    Auto-committing 'project.js'

--- 051-100/064 - Odd period square roots/project.js
+++ 051-100/064 - Odd period square roots/project.js
@@ -2,7 +2,10 @@ function mn(m,d,S){
 	return Math.floor((Math.sqrt(S)+m)/d);
 }
 function approx (){
-
+	var a = mn(0,1,114)
+	var a =[],
+		m = [0],
+		d =[1];
 }
 function main() {
 	'use strict';
