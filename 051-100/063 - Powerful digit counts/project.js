059718a55c9215f5af5bf40ec4b0b80232b4deeb
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-01 15:07:16 -0500

    Auto-committing 'project.js'

--- 051-100/063 - Powerful digit counts/project.js
+++ 051-100/063 - Powerful digit counts/project.js
@@ -9,7 +9,7 @@ function main() {
 		if(String(Math.pow(j,i)).length !== i){
 			bool = false;
 		} else{
-			len = Math.ceil(Math.log(Math.pow(j,i) + 1 ) / Math.LN10);
+
 			while((j > 0) && len === i ){
 				count+=1;
 				console.log(i,j,Math.pow(j,i));
