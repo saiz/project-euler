461914c5d3bf271a4102bd6a85207907951a9cb8
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-02 16:28:04 -0500

    Auto-committing 'project.js'

--- 051-100/065 - Convergents of e/project.js
+++ 051-100/065 - Convergents of e/project.js
@@ -2,16 +2,24 @@ function main(m) {
 	'use strict';
 	m = parseInt(m,10);
 	var a = [1,0,1,1],
-		p = [a[0],(a[0]*a[1]+1)],
-		q = [1,a[1]],
+		p = [],
+		q = [],
 		i = 0, n;
 	while(i< m){
 		n = 3*i +1;
 		a[n] =2*i;
 		n = 3*i +2;
 		a[n] =1;
-		p[i] = a[i]*p[i-1] +p[i-2];
-		q[i] = a[i]*q[i-1] +q[i-2];
+		if(i === 0){
+			p[i] = a[0];
+			q[i] = 1;
+		} else if(i === 1){
+			p[i] = a[i-1]*a[i]+1;
+			q[i] = a[i];
+		} else{
+			p[i] = a[i]*p[i-1] +p[i-2];
+			q[i] = a[i]*q[i-1] +q[i-2];
+		}
 		console.log(a);
 		console.log(p);
 		console.log(q);
