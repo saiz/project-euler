7333962e864e666bbfafb8f2197cda465fbacf82
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-02 13:57:52 -0500

    Auto-committing 'project.js'

--- 051-100/065 - Convergents of e/project.js
+++ 051-100/065 - Convergents of e/project.js
@@ -1,3 +1,18 @@
+function fract(a,b,m,n){
+	var i, k;
+	var i = new BigNumber(a).plus(b),
+		k = new BigNumber(i).plus(b);
+	if(m < n){
+		return fract(k,i,m+1,n);
+	}
+	return [a,b];
+}
+function fraction(n){
+	var a = 3,
+		b = 2;
+	return fract(a,b,1,n);
+}
+
 function main() {
 	'use strict';
 	var i ='test';
