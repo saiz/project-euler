ce36ce00360283ebb71c0e5abbe44068e0e64ac2
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-02 16:08:16 -0500

    Auto-committing 'project.js'

--- 051-100/065 - Convergents of e/project.js
+++ 051-100/065 - Convergents of e/project.js
@@ -2,9 +2,15 @@ function main(n) {
 	'use strict';
 	var a = [2,1],
 		p = [a[0],(a[0]*a[1]+1)],
-		q = [1,a[1]];
+		q = [1,a[1]],
+		n = 0;
 	while(bool){
+		if(n >4 && n%2===1){
+			a[n]
 
+		}
+
+		i+=1;
 	}
 	return;
 }
