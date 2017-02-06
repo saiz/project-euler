63a57840c58b7a9c6ef759278ec7f8a776765f8f
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-06 16:21:03 -0500

    Auto-committing 'project.js'

--- 051-100/066 - Diophantine equation/project.js
+++ 051-100/066 - Diophantine equation/project.js
@@ -86,7 +86,7 @@ function P_n(n,d,a,P,Q){
 	return P[n];
 }
 function diophantine (x,y,d){
-	if(x.toPower(2) - d*y*y === 1){
+	if(x.toPower(2).minus() - d*y*y === 1){
 		return true;
 	}
 	return false;
