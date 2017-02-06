d00f5f6c91839632a75957660e65af2d89b28a30
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-06 13:51:30 -0500

    Auto-committing 'project.js'

--- 051-100/066 - Diophantine equation/project.js
+++ 051-100/066 - Diophantine equation/project.js
@@ -1,4 +1,22 @@
-
+function b(i){
+	if(i ===1){
+		return 0;
+	} else if( (i-1)%3 !== 0){
+		return 1;
+	}
+	return 2*((i-1)/3);
+}
+function A(n,arr){
+	if(n===0){
+		return new BigNumber(b(0));
+	} else if(n===1){
+		return new BigNumber(b(1)*b(0) +1);
+	} else if( arr[n] !== undefined){
+		return new BigNumber(arr[n]);
+	}
+	arr[n] = new BigNumber(b(n)).times(A(n-1,arr)).plus(A(n-2,arr));
+	return arr[n];
+}
 function main(n) {
 	'use strict';
 	n =parseInt(n,10);
