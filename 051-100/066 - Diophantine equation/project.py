cd83890adb4e9f69ca5dd918c6f710e99452242d
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-07 12:05:34 -0500

    Auto-committing 'project.py'

--- 051-100/066 - Diophantine equation/project.py
+++ 051-100/066 - Diophantine equation/project.py
@@ -4,17 +4,5 @@ for d in range(1, 1001):
 	k = int(d**0.5)
 	if k*k == d: continue
 	a, b = k, 1
-	hn, kn = k, 1
-	hp, kp = 1, 0
-	while hn*hn - d*kn*kn != 1:
-		p = (d - a*a) / b
-		q = int(b / (d**0.5 - a))
-		r = p*q - a
-		hn, hp = q*hn + hp, hn
-		kn, kp = q*kn + kp, kn
-		a, b = r, p
-		print q
-	if maxX < hn:
-		maxX, maxD = hn, d
-		print maxX, maxD
+	print a,b,k
 print maxX, maxD
