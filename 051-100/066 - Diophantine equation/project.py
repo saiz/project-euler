3e0f896ddc9d7bec4cc3e1b342e8fc24c1f0187c
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-07 14:22:49 -0500

    Auto-committing 'project.py'

--- 051-100/066 - Diophantine equation/project.py
+++ 051-100/066 - Diophantine equation/project.py
@@ -43,7 +43,7 @@ def main(m) :
 		p['n'] = a['m']*a['n'] + 1
 		q['m'] = 0
 		q['n'] = a['n']
-		x
+		x =
 		while not diophantine(p['n'],q['n'],d):
 			P['m'] = P['n']
 			P['n'] = P_n(a['n'],Q['n'],P['m'])
