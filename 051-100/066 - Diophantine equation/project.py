ec661a565d6e788bfc4bcd7b60e4d1d864024de8
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-07 12:56:01 -0500

    Auto-committing 'project.py'

--- 051-100/066 - Diophantine equation/project.py
+++ 051-100/066 - Diophantine equation/project.py
@@ -1,8 +1,44 @@
 import math
-maxX, maxD = 0, 0
-for d in range(1, 1001):
-	k = int(d**0.5)
-	if k*k == d: continue
-	a, b = k, 1
-	print a,b,k
-print maxX, maxD
+import sys
+
+def diophantine (x,y,d):
+	if x*x - d*y*y == 1:
+		return True
+	return False
+
+def main(m) :
+	m = int(m)
+	d =2
+	max = {
+		'x':0,
+		'd':0
+	}
+	while d <= m:
+		if math.sqrt(d)%1 != 0:
+			n = 0
+			a = {}
+			p = {}
+			q = {}
+			P = {}
+			Q = {}
+			P_n(n,d,a,P,Q)
+			Q_n(n,d,a,P,Q)
+			a_n(n,d,a,P,Q)
+			x = p_n(n,a,p)
+			y = q_n(n,a,q)
+			while(not diophantine(x,y,d)):
+				# print(len(P))
+				P_n(n,d,a,P,Q)
+				Q_n(n,d,a,P,Q)
+				a_n(n,d,a,P,Q)
+				x = p_n(n,a,p)
+				y = q_n(n,a,q)
+				n+=1;
+			if max['x'] < x:
+				max['x'] = x
+				max['d'] = d
+				print()
+		d+=1
+	return max
+
+print(main(sys.argv[1]))
