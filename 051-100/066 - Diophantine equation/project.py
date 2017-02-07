26fe6eef5d7566ffea0b8603054847f8b1cd5cd3
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-07 14:26:24 -0500

    Auto-committing 'project.py'

--- 051-100/066 - Diophantine equation/project.py
+++ 051-100/066 - Diophantine equation/project.py
@@ -6,10 +6,8 @@ def P_n(a,Q,P):
 	return a*Q-P;
 def Q_n(d,P,Q):
 	return (d - int(math.pow(P,2)))/Q
-def p_n(a,p1,p2):
+def D(a,p1,p2):
 	return a*p1 + p2
-def q_n(a,q1,q2):
-	return a*q1 + q2
 def diophantine (x,y,d):
 	if x*x - d*y*y == 1:
 		return True
@@ -52,10 +50,10 @@ def main(m) :
 			Q['n'] = Q_n(d,P['n'],Q['n'])
 			a['m'] = a['n']
 			a['n'] = a_n(a_0,P['n'],Q['n'])
-			temp = p_n(a['n'],P['n'],P['m'])
+			temp = D(a['n'],P['n'],P['m'])
 			p['m'] = p['n']
 			p['n'] = temp
-			temp = q_n(a['n'],Q['n'],Q['m'])
+			temp = D(a['n'],Q['n'],Q['m'])
 			q['m'] = q['n']
 			q['n'] = temp
 			x = p['n']
