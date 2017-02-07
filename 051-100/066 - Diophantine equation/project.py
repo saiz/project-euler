22a0f09eb69d943a492d20786defb63dd167b751
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-07 10:42:43 -0500

    Auto-committing 'project.py'

--- 051-100/066 - Diophantine equation/project.py
+++ 051-100/066 - Diophantine equation/project.py
@@ -6,12 +6,24 @@ def a_n(n,d,a,P,Q):
 		pass
 	if n == 0:
 		a[n] = math.floor( math.sqrt( d ) )
-	return math.floor((a[0]+P[n])/Q[n]);
+		return a[n]
+	a[n] = math.floor((a[0]+P[n])/Q[n])
+	return a[n]
+
+def p_n(n,a,p)
+	try:
+		return p[n]
+	except IndexError:
+		pass
+	if n == 0:
+		p[n] = a[0]
+		return p[n]
+	if n == 1
+		p[n] = a[0]*a[1] +1;
+		return p[n]
+	p[n] = a[n]*p[n-1] +p[n-2];
+	return p[n];
 
 def main() :
 	print("This line will be printed.")
 	return;
-
-arr = [1]
-main()
-print a_n(0,0,arr,0,0)
