915b7058b149a74d99a2534225cea96287acaa1f
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-07 14:14:16 -0500

    Auto-committing 'project.py'

--- 051-100/066 - Diophantine equation/project.py
+++ 051-100/066 - Diophantine equation/project.py
@@ -51,7 +51,9 @@ def main(m) :
 			temp = a_n(a_0,P['n'],Q['n'])
 			a['m'] = a['n']
 			a['n'] = temp
-
+			temp = a_n(a_0,P['n'],Q['n'])
+			a['m'] = a['n']
+			a['n'] = temp
 		}
 		print ('a',a)
 		print ('p',p)
