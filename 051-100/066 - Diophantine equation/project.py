10048cd910142d353847443b27fa2c24d3bc7963
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-07 14:10:41 -0500

    Auto-committing 'project.py'

--- 051-100/066 - Diophantine equation/project.py
+++ 051-100/066 - Diophantine equation/project.py
@@ -4,7 +4,7 @@ def a_n(a,P,Q):
 	return int(math.floor((a + P)/Q))
 def P_n(a,Q,P):
 	return a*Q-P;
-def A_n(a,Q,P):
+def q_n(a,Q,P):
 	return a*Q-P;
 def diophantine (x,y,d):
 	if x*x - d*y*y == 1:
