48a1f1340717af9d88694d8b31139f5b0a93333c
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-08 13:54:55 -0500

    Auto-committing 'project.js'

--- 051-100/068 - Magic 5-gon ring/project.js
+++ 051-100/068 - Magic 5-gon ring/project.js
@@ -34,7 +34,7 @@ function main() {
 		i = min, matrix;
 		while(i <= max){
 			matrix = comb(n, i);
-			if(matrix.length === m){
+			if(matrix.length >= m){
 				console.log(matrix);
 				console.log(n,i);
 				console.log("---");
