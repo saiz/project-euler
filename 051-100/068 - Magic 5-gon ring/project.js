b162e861659f15ca3fec0d0541f9c73e754523c8
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-08 15:58:34 -0500

    Auto-committing 'project.js'

--- 051-100/068 - Magic 5-gon ring/project.js
+++ 051-100/068 - Magic 5-gon ring/project.js
@@ -1,15 +1,3 @@
-function len(n){
-	if(n === 1){
-		return n;
-	}
-	return  n + len(n-1);
-}
-function len2(n,m){
-	if(m === 1){
-		return n;
-	}
-	return  n + len2(n-1, m-1);
-}
 function comb (m, n){
 	var arr = [],
 		i,j,k;
@@ -31,32 +19,7 @@ function comb (m, n){
 	}
 	return arr;
 }
-function hashmap(m,n){
-	var i, j, k,hash={}, arr;
-	for(i=0;i<m.length;i+=1){
-		for(j=0;j<m[i].length;j+=1){
-			if(hash[m[i][j]] === undefined	){
-				hash[m[i][j]] = [];
-			}
-			arr = [];
-			for(k=0;k<m[i].length;k+=1){
-				if(m[i][j] !== m[i][k]){
-					arr.push(m[i][k]);
-				}
-			}
-			hash[m[i][j]].push(arr);
-		}
-	}
-	return hash;
-}
-function limit(hash){
-	var key,
-	copy = hash;
-	for( key in copy){
-		console.log(key,copy[key].length,copy[key]);
-	}
-	return copy;
-}
+
 function parse(n){
 	if(n === '0'){
 		return 10;
@@ -76,32 +39,7 @@ function check(arr, n) {
 	}
 	return true;
 }
-function nextPerm(array){
-	'use strict';
-	var i = array.length -1;
-	while(i>0 && (array[i-1] >= array[i])){
-		i-=1;
-	}
-	if(i<=0){
-		return false;
-	}
-	var j = array.length -1;
-	while(array[j] <= array[i-1]){
-		j-=1;
-	}
-	var temp = array[i-1];
-	array[i-1] = array[j];
-	array[j] = temp;
-	j = array.length - 1;
-	while (i < j) {
-        temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-        i+=1;
-        j-=1;
-    }
-    return true;
-}
+
 function main() {
 	'use strict';
 	var n = 10, // number 1 - n
@@ -114,19 +52,7 @@ function main() {
 		// dict = hashmap(matrix,n);
 		// limit(dict);
 		console.log(matrix)
-		// while(i > 0){
-		// 	str =String(i);
-		// 	arr  = [str.slice(0,3),str.slice(3,6),str.slice(6,9)];
-		// 	//console.log(arr);
-		// 	if(check(arr,len)){
-		// 		console.log([val(arr[0]),val(arr[1]),val(arr[2])]);
-		// 		console.log(arr);
-		// 		console.log("--")
-		// 	}
-		// 	i-=1;
 
-		// }
-		//console.log(dict);
 
 	return;
 }
