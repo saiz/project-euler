d662a2f1d4051413ced8a034f8d0f654d47b2849
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-08 13:36:08 -0500

    Auto-committing 'project.js'

--- 051-100/068 - Magic 5-gon ring/project.js
+++ 051-100/068 - Magic 5-gon ring/project.js
@@ -21,8 +21,8 @@ function comb (n){
 }
 function main() {
 	'use strict';
-	var n = 6, // number 1 - n
-		m = 3, // number of pairs
+	var n = 9, // number 1 - n
+		m = 5, // number of pairs
 		d = 3, // length of comb
 		min = len(d) + m,
 		max = len2(n,3),
