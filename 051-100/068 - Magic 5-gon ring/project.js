68a6bfe44df0c91c6b9da071575e6350077f532f
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-10 12:24:13 -0500

    Auto-committing 'project.js'

--- 051-100/068 - Magic 5-gon ring/project.js
+++ 051-100/068 - Magic 5-gon ring/project.js
@@ -88,14 +88,10 @@ function main() {
 			bool2 = true;
 			for(j=0;j<h.length;j+=1){
 				side =next(h,j);
-				index= l.indexOf(side,h[j]);
+				index= l.indexOf(side+h[j]);
 				if (index > -1) {
 				    array.splice(index, 1);
 				}
-				console.log(side,h[j]);
-				// if(side[0] +side[1] +h[j] !== len){
-				// 	bool2= false;
-				// }
 			}
 			if(bool2){
 				console.log(h);
