163af32d20f2fc1a533fbf69bba7f178c9c9ef18
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-16 14:04:22 -0500

    Auto-committing 'project.js'

--- 051-100/069 - Totient maximum/project.js
+++ 051-100/069 - Totient maximum/project.js
@@ -1,3 +1,16 @@
+function isPrime3(n) {
+    'use strict';
+    if (isNaN(n) || !isFinite(n) || n%1 || n<2){ return false;}
+    if (n%2===0) {return (n===2);}
+    if (n%3===0) {return (n===3);}
+    var m=Math.sqrt(n), i;
+    for (i=5;i<=m;i+=6) {
+        if (n%i===0)     {return false;}
+        if (n%(i+2)===0) {return false;}
+    }
+    return true;
+}
+
 function main() {
 	'use strict';
 	var i ='test';
