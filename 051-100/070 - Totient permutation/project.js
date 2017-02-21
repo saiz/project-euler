91ba5688fa33b00540450fc61ed28ba7c6524516
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-02-21 14:03:32 -0500

    Auto-committing 'project.js'

--- 051-100/070 - Totient permutation/project.js
+++ 051-100/070 - Totient permutation/project.js
@@ -1,3 +1,38 @@
+function eratosthenes(n) {
+    // Eratosthenes algorithm to find all primes under n
+    var array = [], upperLimit = Math.sqrt(n), output = [], i, j;
+    // Make an array from 2 to (n - 1)
+    for (i = 0; i < n; i++) {
+        array.push(true);
+    }
+    // Remove multiples of primes starting from 2, 3, 5,...
+    for (i = 2; i <= upperLimit; i++) {
+        if (array[i]) {
+            for (j = i * i; j < n; j += i) {
+                array[j] = false;
+            }
+        }
+    }
+    // All array[i] set to true are primes
+    for (i = 2; i < n; i++) {
+        if(array[i]) {
+            output.push(i);
+        }
+    }
+    return output;
+}
+
+function phi(n) {
+	var res = 1,
+	sieve = eratosthenes(n),
+	i = 0;
+	while(res * sieve[i] < n){
+	    res *= sieve[i];
+	    i++;
+	}
+	return res;
+}
+
 function main() {
 	'use strict';
 	var i ='test';
