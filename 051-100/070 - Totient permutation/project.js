cb8cc82b68adffd7d4536fe4e6c6347ca2979296
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-05-16 13:42:53 -0400

    Auto-committing 'project.js'

--- 051-100/070 - Totient permutation/project.js
+++ 051-100/070 - Totient permutation/project.js
@@ -32,7 +32,25 @@ function perm(str1, str2) {
 
     return a === b;
 }
+function isPrime3(n) {
+	'use strict';
+	if (isNaN(n) || !isFinite(n) || n%1 || n<2){ return false;}
+	if (n%2===0) {return (n===2);}
+	if (n%3===0) {return (n===3);}
+	var m=Math.sqrt(n), i;
+	for (i=5;i<=m;i+=6) {
+		if (n%i===0)     {return false;}
+		if (n%(i+2)===0) {return false;}
+	}
+	return true;
+}
 function getAllFactorsFor(integer) {
+	if(integer ===1){
+		return [];
+	}
+	if(isPrime3(integer)){
+		return [1, integer];
+	}
 	var factors = [],
 	quotient = 0;
 
