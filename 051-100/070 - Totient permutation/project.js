7593da40d37db197e658a78a329c26865042317e
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-05-16 11:58:20 -0400

    Auto-committing 'project.js'

--- 051-100/070 - Totient permutation/project.js
+++ 051-100/070 - Totient permutation/project.js
@@ -69,8 +69,9 @@ function main() {
 		i=1;
 	while(i<n){
 		temp = phi(i);
-		if(is_permutation(i,temp) && (min > i/temp)
+		if(is_permutation(i,temp) && (min > i/temp){
 			min = i/temp;
+			console.log(min, i, temp);
 		}
 		i+=1;
 	}
