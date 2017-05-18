f29878e333ae86dd0d5b27e3d29877cc29d605ad
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-05-18 14:49:13 -0400

    Auto-committing 'project.js'

--- 051-100/070 - Totient permutation/project.js
+++ 051-100/070 - Totient permutation/project.js
@@ -44,7 +44,7 @@ function main() {
 		minratio = Infinity,
 		minphi = Infinity,
 		min = Infinity,
-		primes = eratosthenes(Math.sqrt(max)),
+		primes = eratosthenes(max/),
 		i=0,j;
 		console.log(Math.sqrt(max))
 	while(i<primes.length){
