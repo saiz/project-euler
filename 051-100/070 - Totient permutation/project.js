1f3baa3a798d678003cfdb15adda26e4ecf9f19b
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-05-18 14:22:05 -0400

    Auto-committing 'project.js'

--- 051-100/070 - Totient permutation/project.js
+++ 051-100/070 - Totient permutation/project.js
@@ -39,7 +39,8 @@ function main() {
 			var phi =(primes[i] - 1) * (primes[j] - 1),
 				ratio =primes[i]*primes[j]/phi;
 			if(ratio < minratio){
-				ratio = minr
+				ratio = minratio
+				min =
 			}
 
 			j+=1;
