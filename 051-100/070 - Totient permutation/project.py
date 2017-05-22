913c33371a5178a5bda7a1076a5806f579e1ade9
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-05-22 12:23:02 -0400

    Auto-committing 'project.py'

--- 051-100/070 - Totient permutation/project.py
+++ 051-100/070 - Totient permutation/project.py
@@ -10,5 +10,15 @@ def isperm( str1, str2 ):
 		_str = _str.replace(str1[i],"",1)
 		i+=1;
 	return bool
+def primes_sieve2(limit):
+    a = [True] * limit                          # Initialize the primality list
+    a[0] = a[1] = False
+
+    for (i, isprime) in enumerate(a):
+        if isprime:
+            yield i
+            for n in xrange(i*i, limit, i):     # Mark factors non-prime
+                a[n] = False
 
 print isperm('sadsa', 'sasad')
+print primes_sieve2(100)
