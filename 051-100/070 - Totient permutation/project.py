d7d22821090ad8bf5af38128f34dce358f50e6bf
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-05-22 13:03:01 -0400

    Auto-committing 'project.py'

--- 051-100/070 - Totient permutation/project.py
+++ 051-100/070 - Totient permutation/project.py
@@ -42,7 +42,10 @@ while i< len(primes):
 		phi =  (primes[i] -1 ) * (primes[j] -1 )
 		# print n,phi
 		ratio = n / phi
-		if isperm(str(n),str(phi)):
+		if  ratio < min_ratio:
+			min = n
+			min_ratio = ratio
+			min_phi = phi
 			print True
 		# if isperm(str(n),str(phi)) and ratio < min_ratio:
 		# 	min = n
