f3851dcb4a645cb1470a8911fe2ab1e95a2c689f
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-05-22 12:31:35 -0400

    Auto-committing 'project.py'

--- 051-100/070 - Totient permutation/project.py
+++ 051-100/070 - Totient permutation/project.py
@@ -13,17 +13,20 @@ def isperm( str1, str2 ):
 	return bool
 def primes_sieve(limit):
     limitn = limit+1
-    not_prime = False * limitn
+    not_prime = set()
     primes = []
+
     for i in range(2, limitn):
-        if not_prime[i]:
+        if i in not_prime:
             continue
-        for f in xrange(i*2, limitn, i):
-            not_prime[f] = True
+
+        for f in range(i*2, limitn, i):
+            not_prime.add(f)
 
         primes.append(i)
 
     return primes
+
 
 limit =  10 ** 7
 print limit
