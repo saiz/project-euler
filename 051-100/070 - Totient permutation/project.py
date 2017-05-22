77304a5bb26758a74cedf06fbdc28e062e02f7c4
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-05-22 12:15:24 -0400

    Auto-committing 'project.py'

--- 051-100/070 - Totient permutation/project.py
+++ 051-100/070 - Totient permutation/project.py
@@ -2,9 +2,12 @@ def isperm( str1, str2 ):
 	if (len(str1) != len(str2)):
 		return False
 	i = 0
-	while( i < len(str1)):
+	bool = true
+	while( i < len(str1) and bool):
 		print str1[i]
+		if !(str1[i] in str2):
+			bool = false
 		i+=1;
-	return True
+	return bool
 
 print isperm('adsad', 'sasad')
