dec70685675e1fe1d8c2034122992842ae86113f
Andres Diaz-Borda <Andres_Diaz-Borda@mckinsey.com>
2017-05-22 12:05:37 -0400

    Auto-committing 'project.py'

--- 051-100/070 - Totient permutation/project.py
+++ 051-100/070 - Totient permutation/project.py
@@ -1,6 +1,6 @@
-def functionname( parameters ):
-   "function_docstring"
-   function_suite
-   return [expression]
+def printme( str ):
+   "This prints a passed string into this function"
+   print str
+   return
 
 functionname('ad')
